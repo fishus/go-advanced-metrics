ecd39658a585d2abc6c185f2c03d757efee3c5c2
Slava Ershov <vyacheslav.ershov@gmail.com>
2023-12-09 23:48:53 +0300

    Refactoring the ValueHandler test for using suite

--- internal/handlers/value_test.go
+++ internal/handlers/value_test.go
@@ -3,150 +3,110 @@ package handlers
 import (
 	"github.com/fishus/go-advanced-metrics/internal/metrics"
 	"github.com/go-resty/resty/v2"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func testValueRequest(t *testing.T, client *resty.Client, method, url string) *resty.Response {
-	resp, err := client.R().
-		SetHeader("Content-Type", "text/plain; charset=utf-8").
-		Execute(method, url)
-	require.NoError(t, err)
+type ValueHandlerSuite struct {
+	suite.Suite
+	ts     *httptest.Server
+	client *resty.Client
+}
 
+func (s *ValueHandlerSuite) SetupSuite() {
+	s.ts = httptest.NewServer(ServerRouter())
+	s.client = resty.New()
+}
+
+func (s *ValueHandlerSuite) TearDownSuite() {
+	s.ts.Close()
+}
+
+func (s *ValueHandlerSuite) SetupTest() {
+	storage = metrics.NewMemStorage()
+	_ = storage.AddCounter("a", 5)
+	_ = storage.SetGauge("a", 1.5)
+}
+
+func (s *ValueHandlerSuite) requestValue(url string) *resty.Response {
+	resp, err := s.client.R().
+		SetHeader("Content-Type", "text/plain; charset=utf-8").
+		Get(url)
+	s.Require().NoError(err)
 	return resp
 }
 
-func runTestServer() *httptest.Server {
-	return httptest.NewServer(ServerRouter())
-}
-
-func TestValueHandler(t *testing.T) {
-	ts := runTestServer()
-	defer ts.Close()
-
-	client := resty.New()
-
+func (s *ValueHandlerSuite) TestValueHandler() {
 	testCases := []struct {
 		name   string
-		setUp  func()
-		method string
 		url    string
 		want   string
 		status int
 	}{
 		{
-			name: "Positive case: Counter",
-			setUp: func() {
-				storage = metrics.NewMemStorage()
-				_ = storage.AddCounter("a", 5)
-			},
-			method: http.MethodGet,
+			name:   "Positive case: Counter",
 			url:    "/value/counter/a",
 			want:   "5",
 			status: http.StatusOK,
 		},
 		{
-			name: "Positive case: Gauge",
-			setUp: func() {
-				storage = metrics.NewMemStorage()
-				_ = storage.SetGauge("a", 1.5)
-			},
-			method: http.MethodGet,
+			name:   "Positive case: Gauge",
 			url:    "/value/gauge/a",
 			want:   "1.5",
 			status: http.StatusOK,
 		},
 		{
-			name: "Positive case: Counter not found",
-			setUp: func() {
-				storage = metrics.NewMemStorage()
-			},
-			method: http.MethodGet,
-			url:    "/value/counter/a",
+			name:   "Positive case: Counter not found",
+			url:    "/value/counter/x",
 			want:   "",
 			status: http.StatusNotFound,
 		},
 		{
-			name: "Positive case: Gauge not found",
-			setUp: func() {
-				storage = metrics.NewMemStorage()
-			},
-			method: http.MethodGet,
-			url:    "/value/gauge/a",
+			name:   "Positive case: Gauge not found",
+			url:    "/value/gauge/x",
 			want:   "",
 			status: http.StatusNotFound,
 		},
 		{
-			name: "Negative case: Incorrect metric type",
-			setUp: func() {
-				storage = metrics.NewMemStorage()
-				_ = storage.AddCounter("a", 5)
-				_ = storage.SetGauge("b", 1.5)
-			},
-			method: http.MethodGet,
+			name:   "Negative case: Incorrect metric type",
 			url:    "/value/histogram/h",
 			want:   "",
 			status: http.StatusBadRequest,
 		},
 		{
-			name: "Negative case: Wrong url #1",
-			setUp: func() {
-				storage = metrics.NewMemStorage()
-			},
-			method: http.MethodGet,
+			name:   "Negative case: Wrong url #1",
 			url:    "/value/counter/",
 			want:   "",
 			status: http.StatusNotFound,
 		},
 		{
-			name: "Negative case: Wrong url #2",
-			setUp: func() {
-				storage = metrics.NewMemStorage()
-			},
-			method: http.MethodGet,
+			name:   "Negative case: Wrong url #2",
 			url:    "/value/counter",
 			want:   "",
 			status: http.StatusNotFound,
 		},
 		{
-			name: "Negative case: Wrong url #3",
-			setUp: func() {
-				storage = metrics.NewMemStorage()
-			},
-			method: http.MethodGet,
+			name:   "Negative case: Wrong url #3",
 			url:    "/value/",
 			want:   "",
 			status: http.StatusNotFound,
 		},
 		{
-			name: "Negative case: Wrong url #4",
-			setUp: func() {
-				storage = metrics.NewMemStorage()
-			},
-			method: http.MethodGet,
+			name:   "Negative case: Wrong url #4",
 			url:    "/value",
 			want:   "",
 			status: http.StatusNotFound,
 		},
 		{
-			name: "Negative case: Wrong url #5",
-			setUp: func() {
-				storage = metrics.NewMemStorage()
-			},
-			method: http.MethodGet,
+			name:   "Negative case: Wrong url #5",
 			url:    "/value//",
 			want:   "",
 			status: http.StatusNotFound,
 		},
 		{
-			name: "Negative case: Metric type not specified",
-			setUp: func() {
-				storage = metrics.NewMemStorage()
-			},
-			method: http.MethodGet,
+			name:   "Negative case: Metric type not specified",
 			url:    "/value//name",
 			want:   "",
 			status: http.StatusBadRequest,
@@ -154,16 +114,17 @@ func TestValueHandler(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.setUp()
-
-			resp := testValueRequest(t, client, tc.method, ts.URL+tc.url)
-
-			assert.Equal(t, tc.status, resp.StatusCode())
+		s.Run(tc.name, func() {
+			resp := s.requestValue(s.ts.URL + tc.url)
+			s.Equal(tc.status, resp.StatusCode())
 
 			if resp.StatusCode() == http.StatusOK {
-				assert.Equal(t, tc.want, string(resp.Body()))
+				s.Equal(tc.want, string(resp.Body()))
 			}
 		})
 	}
+}
+
+func TestValueHandlerSuite(t *testing.T) {
+	suite.Run(t, new(ValueHandlerSuite))
 }
