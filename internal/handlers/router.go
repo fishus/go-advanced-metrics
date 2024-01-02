b5f899f446d57acb7c07a14fdaff2780baba52aa
Slava Ershov <vyacheslav.ershov@gmail.com>
2024-01-02 16:01:01 +0300

    Increment 8: The server can accept gzip encoded request

--- internal/handlers/router.go
+++ internal/handlers/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
+	mw "github.com/fishus/go-advanced-metrics/internal/handlers/middleware"
 	"github.com/fishus/go-advanced-metrics/internal/logger"
 )
 
@@ -13,6 +14,7 @@ func ServerRouter() chi.Router {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.RequestID)
+	r.Use(mw.Decompress)
 	r.Use(middleware.RequestLogger(&logger.LogFormatter{}))
 
 	r.Post("/update/", UpdateMetricsHandler)
