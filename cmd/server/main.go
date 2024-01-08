aafc666bf017b5427211ead087fbd5f392ef440a
Slava Ershov <vyacheslav.ershov@gmail.com>
2024-01-08 21:16:58 +0300

    Increment 9: Add the server saves metric values at intervals

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"time"
 
 	"go.uber.org/zap"
 
 	"github.com/fishus/go-advanced-metrics/internal/handlers"
 	"github.com/fishus/go-advanced-metrics/internal/logger"
+	"github.com/fishus/go-advanced-metrics/internal/metrics"
 )
 
 var config Config
@@ -17,6 +20,7 @@ func main() {
 		panic(err)
 	}
 	defer logger.Log.Sync()
+	go saveMetricsAtIntervals()
 	runServer()
 }
 
@@ -25,5 +29,32 @@ func runServer() {
 	err := http.ListenAndServe(config.serverAddr, handlers.ServerRouter())
 	if err != nil {
 		logger.Log.Panic(err.Error(), zap.String("event", "start server"))
+	}
+}
+
+func saveMetricsAtIntervals() {
+	if config.storeInterval <= 0 || config.fileStoragePath == "" {
+		return
+	}
+
+	s := handlers.Storage()
+	s.Filename = config.fileStoragePath
+
+	now := time.Now()
+	storeAfter := now.Add(config.storeInterval)
+	for {
+		now = time.Now()
+		if now.After(storeAfter) {
+			storeAfter = now.Add(config.storeInterval)
+			err := s.Save()
+			if !errors.Is(err, metrics.ErrEmptyFilename) {
+				if err != nil {
+					logger.Log.Error(err.Error(), zap.String("event", "save metrics into file"))
+				} else {
+					logger.Log.Debug("Metric values saved into file", zap.String("event", "save metrics into file"))
+				}
+			}
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
