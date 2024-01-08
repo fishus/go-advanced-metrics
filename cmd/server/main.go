76bb9203843bc8082fe82eefbc8232284ad30b33
Slava Ershov <vyacheslav.ershov@gmail.com>
2024-01-08 21:21:34 +0300

    Increment 9: Add loading of metrics values from a file before starting the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,6 +19,7 @@ func main() {
 		panic(err)
 	}
 	defer logger.Log.Sync()
+	loadMetricsFromFile()
 	go saveMetricsAtIntervals()
 	runServer()
 }
@@ -28,6 +29,24 @@ func runServer() {
 	err := http.ListenAndServe(config.serverAddr, handlers.ServerRouter())
 	if err != nil {
 		logger.Log.Panic(err.Error(), zap.String("event", "start server"))
+	}
+}
+
+func loadMetricsFromFile() {
+	if !config.isReqRestore || config.fileStoragePath == "" {
+		return
+	}
+
+	s := handlers.Storage()
+	s.Filename = config.fileStoragePath
+
+	err := s.Load()
+	if !errors.Is(err, metrics.ErrEmptyFilename) {
+		if err != nil {
+			logger.Log.Warn(err.Error(), zap.String("event", "load metrics from file"))
+			return
+		}
+		logger.Log.Debug("Metric values loaded from file", zap.String("event", "load metrics from file"))
 	}
 }
 
